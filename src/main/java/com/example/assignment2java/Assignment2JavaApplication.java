a45b5de96f50d3a8c7c42e0ca91e89ef8291a31f
bacok <ikgarpun@gmail.com>
2025-12-28 19:39:34 +0500

    init commit

---
+++ src/main/java/com/example/assignment2java/Assignment2JavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment2java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment2JavaApplication.class, args);
+    }
+
+}
